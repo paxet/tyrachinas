842270997fda0be03dbaa6e23b11eb2b29881d61
Javier Caballero <jcaballero@empretal.com>
2016-03-01 17:59:11 +0100

    Refactor

--- config.py
+++ config.py
@@ -9,12 +9,11 @@ SECRET_KEY = b64encode(os.urandom(64)).decode('utf-8')[:30]
 WTF_CSRF_KEY = b64encode(os.urandom(64)).decode('utf-8')[:30]
 WTF_CSRF_SECRET_KEY = b64encode(os.urandom(64)).decode('utf-8')[:30]
 
-MAIL_SERVER = '172.26.26.32'
+MAIL_SERVER = 'localhost'
 MAIL_PORT = '25'
 MAIL_USE_TLS = False
 MAIL_USE_SSL = False
-MAIL_DEFAULT_SENDER = 'marketing@ainia.es'
-MAIL_DESTINATARIO_PRUEBAS = 'fjllopis@ainia.es'
+MAIL_DEFAULT_SENDER = 'info@tyrachinas.io'
 
 UPLOAD_FOLDER = 'uploads'
 FILE_FORMATS = ['zip', '7z', 'tgz', 'gz', 'bz', 'rar']
