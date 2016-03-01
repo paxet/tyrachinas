f55af64d1df94ae9aa3e1919af3b91bdaf4bd294
Javier Caballero <jcaballero@empretal.com>
2016-03-01 17:53:25 +0100

    Upload and download are working

--- webapp/controllers.py
+++ webapp/controllers.py
@@ -1,5 +1,5 @@
 import os, uuid
-from flask import Blueprint, render_template, redirect, url_for, flash, Markup, current_app
+from flask import Blueprint, render_template, redirect, url_for, flash, Markup, current_app, send_file, send_from_directory
 from werkzeug.utils import secure_filename
 from webapp.forms import FormResource
 from webapp.models import Resource
@@ -24,22 +24,34 @@ def resources():
         if form.attachment.has_file():
             remote_filename = secure_filename(form.attachment.data.filename)
             local_filename = str(uuid.uuid4())
-            folder = current_app.config['UPLOAD_FOLDER']
+            folder = os.path.join(os.getcwd(), current_app.config['UPLOAD_FOLDER'])
             if not os.path.exists(folder):
                 os.makedirs(folder)
-            file_path = folder + '/' + local_filename
+            file_path = os.path.join(folder, local_filename)
+            #TODO Encrypt file
             form.attachment.data.save(file_path)
-            Resource.create(filename=remote_filename,
+            res = Resource.create(filename=remote_filename,
                             description=form.description.data,
                             path=file_path,
                             mimetype=form.attachment.data.mimetype,
                             email_owner=form.email_owner.data,
                             email_receiver=form.email_receiver.data)
-            # TODO Send email to inform
-            flash(Markup('Resource added'))
+            # TODO Send email to owner and receiver
+            download_url = url_for('root.download', file_id=res.id)
+            flash(Markup('Resource added: <a href="{}">Download</a>'.format(download_url)))
             form = FormResource()
         else:
             flash(Markup('Can\'t do it without attachment'))
     return render_template('index.html',
                            form=form,
                            formats=htmlinput_accepted_formats())
+
+
+@listener.route("/download/<int:file_id>", methods=['GET'])
+def download(file_id):
+    #TODO Decrypt file
+    res = Resource.get(Resource.id == file_id)
+    return send_file(filename_or_fp=res.path,
+                     mimetype=res.mimetype,
+                     as_attachment=True,
+                     attachment_filename=res.filename)
